ee2eab2afa81b0d6ab43f7260bab94abd575910e
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2022-05-15 08:15:21 -0500

    edited active blcoks
    added and removed a few blocks

--- config.h
+++ config.h
@@ -1,37 +1,20 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	/* {"⌨", "sb-kbselect", 0, 30}, */
-//	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-//	{"",	"sb-tasks",	10,	26},
-//	{"",	"sb-music",	0,	11},
-//	{"",	"sb-pacpackages",	0,	8},
-//	{"",	"sb-news",		0,	6},
-	/* {"",	"sb-price lbc \"LBRY Token\" 📚",			9000,	22}, */
-	/* {"",	"sb-price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
-	/* {"",	"sb-price link \"Chainlink\" 🔗",			300,	25}, */
-	/* {"",	"sb-price xmr \"Monero\" 🔒",			9000,	24}, */
-	/* {"",	"sb-price eth Ethereum 🍸",	9000,	23}, */
-	/* {"",	"sb-price btc Bitcoin 💰",				9000,	21}, */
-//	{"",	"sb-torrent",	20,	7},
-	/* {"",	"sb-memory",	10,	14}, */
-	/* {"",	"sb-cpu",		10,	18}, */
-	/* {"",	"sb-moonphase",	18000,	17}, */
-//	{"",	"sb-forecast",	18000,	5},
-//	{"",	"sb-mailbox",	180,	12},
-//	{"",	"sb-nettraf",	1,	16},
-//	{"",	"sb-volume",	0,	10},
-//	{"",	"sb-battery",	5,	3},
-//	{"",	"sb-clock",	60,	1},
-//	{"",	"sb-internet",	5,	4},
-//	{"",	"sb-help-icon",	0,	15},
-//	{"", 	"sb-internet", 0, 0},
-//	{"", 	"sb-volume", 0, 10},
-	{"", 	"echo $USER at $HOSTNAME", 0, 0},
-	{"", 	"echo '|'", 0, 1},
-	{"", 	"echo $(date +%D)", 0, 2},
-	{"",	"echo '|'", 0, 3},
-	{"", 	"echo $(date +%r)", 1, 4}
+	{"", 	"internet.sh", 3, 0},
+	{"",    "echo '|'", 0, 1},
+	{"",    "volume.sh", 1, 2},
+	{"",    "echo '|'", 0, 3},
+	{"",    "notif-history.sh", 10, 4},
+	{"",    "echo '|'", 0, 5},
+	{"",	"battery.sh", 3, 6},
+	{"", 	"echo '|'", 0, 7},
+	{"",    "echo $USER at $HOSTNAME", 0, 8},
+	{"",    "echo '|'", 0, 9},
+	{"",    "echo $(date +%D)", 60, 10},
+	{"",    "echo '|'", 0, 11},
+	{"",    "echo $(date +%r)   ", 1, 12},
+//	{"",	"easyshot.sh", 0, 13},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
