5b9bd6a2fbc438407a2ab8a94e8621b694f1da1f
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2022-05-16 09:43:34 -0500

    replaced easyshot with ssc

--- easyshot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# easyshot
-#taking screenshots with scrot
-easyshot="📷"
-shot=$(scrot -s Pictures/Screenshots/easyshot.png)
-case $BLOCK_BUTTON in
-	1) scrot -s Pictures/Screenshots/easyshot.png;;
-esac
-echo "$easyshot $shot"
