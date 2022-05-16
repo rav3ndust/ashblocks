0c0c3d0d44ae5fcfe3d16cee48555c070930e165
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2022-05-16 10:49:00 -0500

    commented out unused function

--- launchssc.sh
+++ launchssc.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #SSC=$(ssc)
 cam='📷'
-_ssc() {
-	$SSC
-}
+#_ssc() {
+#	$SSC
+#}
 case $BLOCK_BUTTON in
 	1) ssc ;;
 	3) notify-send "ssc" "snap a screenshot, save it to your clipboard." ;;
